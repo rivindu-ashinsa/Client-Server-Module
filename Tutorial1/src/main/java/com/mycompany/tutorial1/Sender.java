5a86aa072d9ebfa2e06b547bdc7b351ab3be5836
Rivindu Ashinsa <ashinsa.rivindu@gmail.com>
2026-01-28 12:06:23 +0530

    Added System logs - logging class

--- Tutorial1/src/main/java/com/mycompany/tutorial1/Sender.java
+++ Tutorial1/src/main/java/com/mycompany/tutorial1/Sender.java
@@ -4,18 +4,25 @@
  */
 package com.mycompany.tutorial1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
 public class Sender {
+    private static final Logger logger = Logger.getLogger(Sender.class.getName());
+
     public Message createMessage(Message message){
         Message message1 = new Message("This is the message");
-        System.out.println("Message Created" + message.getContent());
+        logger.log(Level.INFO, "Message Craeted " + message.getContent());
+        System.out.println("Message Created " + message.getContent());
         return message1; 
     }
     public void sendMessage(Receiver receiver, Message message){
         System.out.println("Message Sending");
+        logger.log(Level.INFO, "Message Sending " + message.getContent());
         receiver.receiveMessage(message);
     }
 }
--- Tutorial1/src/main/java/com/mycompany/tutorial1/Tutorial1.java
+++ Tutorial1/src/main/java/com/mycompany/tutorial1/Tutorial1.java
@@ -4,19 +4,23 @@
 
 package com.mycompany.tutorial1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author User
  */
 public class Tutorial1 {
+    private static final Logger logger = Logger.getLogger(Tutorial1.class.getName());
 
     public static void main(String[] args) {
+      
         Sender sender = new Sender();
         Receiver receiver = new Receiver();
         
         Message message = sender.createMessage(new Message("Thi s is my message"));
         sender.sendMessage(receiver, message);
-        System.out.println("Message sent");
+        logger.log(Level.SEVERE,"Message sent");
         
     }
 }
